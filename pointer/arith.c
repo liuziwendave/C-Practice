69825ce0f3fbd47e67a315294f73f0428bbd2c68
Dave <liuziwendave@gmail.com>
2017-02-26 02:02:36 +1100

    In Australia

---
+++ pointer/arith.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char **argv){
+   int array[]={1,2,3,4,5}; 
+   int *ptr=array;
+  
+   int numbers_of_elements=sizeof(array)/sizeof(int);
+   for(int i=0; i<numbers_of_elements;i++){
+      printf("%d\n", *ptr);
+      ptr++;
+   }
+
+   return EXIT_SUCCESS;
+}
+
