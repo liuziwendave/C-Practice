ab8f918f9352fa2f5c672dea49dbe3405c1cd920
Dave <liuziwendave@gmail.com>
2017-02-26 18:16:10 +1100

    In Australia

--- pointer/arith.c
+++ pointer/arith.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> 
+#include <stdlib.h> 
 
 int main (int argc, char **argv){
    int array[]={1,2,3,4,5}; 
-   int *ptr=array;
   
    int numbers_of_elements=sizeof(array)/sizeof(int);
    for(int i=0; i<numbers_of_elements;i++){
@@ -13,4 +12,3 @@ int main (int argc, char **argv){
 
    return EXIT_SUCCESS;
 }
-
